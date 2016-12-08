1a1ee9198a3681018de927cd1fdde4e97b32f898
ismilesky <fighter_kongfei@126.com>
2016-12-08 15:54:24 +0800

    Initial Commit

---
+++ MultistageVoice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultistageVoice
+//
+//  Created by 哈哈 on 16/12/8.
+//  Copyright © 2016年 Felix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultistageVoice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultistageVoice
+//
+//  Created by 哈哈 on 16/12/8.
+//  Copyright © 2016年 Felix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
